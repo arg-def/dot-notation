1dbeed063ec8c9e1b69ce6cabc6ba380ce15aab7
Marcos <contact@themgoncalves.com>
2020-07-13 19:42:14 +0200

    fix: add deprecation notice

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import parse from './parse';
 import parseKey from './parse-key';
 import { IKeySource, IKeyFromPathResult } from './interfaces';
 
+if (['development', 'dev'].includes(process.env.NODE_ENV)) {
+  console.warn(
+    'Warning: "@arg-def/dot-notation" has been moved to "@cookbook/dot-notation".\n\n' +
+      'Repository & Documentation: https://bit.ly/3ewtm3u',
+  );
+}
+
 export { pick, parse, parseKey, IKeySource, IKeyFromPathResult };
 
 export default {
